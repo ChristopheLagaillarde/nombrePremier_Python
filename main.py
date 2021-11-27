bce11fc49ca5a976aac58eb317f0e9035562d563
ChristopheLagaillarde <chrislag94@gmail.com>
2021-11-27 22:01:40 +0400

    Le programme nombre premier

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+sys.tracebacklimit = 0
+
+try:
+    a = int(input("saisir un nombre"))
+except ValueError:
+    print("Saisie incorrecte!")
+
+
+try:
+    for i in range(2, a-1):
+        temp = a % i
+        if(temp == 0):
+            print("Ce nombre n'est pas premier")
+            break
+    else:
+        print("Ce nombre est premier !")
+except NameError:
+    print()
